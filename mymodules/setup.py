5b6db750d82e80f7babfcf1d2b4b137a7accdf6c
shurupyan <shurupyan@mail.ru>
2019-01-09 22:51:31 +0300

    chapter 5

--- mymodules/setup.py
+++ mymodules/setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup
 
-setup(name='vsearch', version='1.0', description='The Head First Python Search tools', author='HF Python 2e', author_email='hfpy2e@gmail.com',url='headfirstlabs.com', py_mpdules=['vsearch'],)
+setup(name='vsearch', version='1.0.1', description='The Head First Python Search tools', author='HF Python 2e', author_email='hfpy2e@gmail.com',url='headfirstlabs.com', py_modules=['vsearch'],)
