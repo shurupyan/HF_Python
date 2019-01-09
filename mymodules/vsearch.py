8cc51bc5f3eff76be9d730df917d150da8b9d01e
shurupyan <shurupyan@mail.ru>
2019-01-09 19:39:14 +0300

    chap4 complete

--- mymodules/vsearch.py
+++ mymodules/vsearch.py
@@ -1,9 +1,9 @@
-def search4vowels(phrase:str) -> set:
+def search4vowels(phrase: str) -> set:
     """ Returns vowels found in specified string """
     vowels = set('aeiou')
     return vowels.intersection(set(phrase))
 
 
-def search4letters(phrase:str, letters:str='aeiou') -> set:
+def search4letters(phrase: str, letters: str='aeiou') -> set:
     """ Returns letters found in specified string """
     return set(letters).intersection(set(phrase))
