0a30dc72d21bafef6e159156e31e294d768318d8
shurupyan <shurupyan@mail.ru>
2019-01-16 23:28:23 +0300

    chap10 continue

--- chap10/simple_webapp.py
+++ chap10/simple_webapp.py
@@ -1,4 +1,5 @@
 from flask import Flask, session
+from checker import check_logged_in
 
 app = Flask(__name__)
 
@@ -10,16 +11,19 @@ def hello() -> str:
 
 
 @app.route('/page1')
+@check_logged_in
 def page1() -> str:
     return 'This is the page 1'
 
 
 @app.route('/page2')
+@check_logged_in
 def page2() -> str:
     return 'This is the page 2'
 
 
 @app.route('/page3')
+@check_logged_in
 def page3() -> str:
     return 'This is the page 3'
 
@@ -35,14 +39,6 @@ def do_logout() -> str:
     if 'logged_in' in session:
         session.pop('logged_in')
     return 'You are now logged out'
-
-
-@app.route('/status')
-def check_status() -> str:
-    if 'logged_in' in session:
-        return 'You are currently logged in'
-    else:
-        return 'You are NOT logged in'
 
 
 if __name__ == '__main__':
